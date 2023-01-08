10787380cdc305b96df3928e0edee0b46862dd02
Pashatishinin <99612110+Pashatishinin@users.noreply.github.com>
2023-01-08 20:44:57 +0100

    generate_users

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request
 import urllib
+from random import randint
+import csv
+from urllib.parse import urlparse
+from http.cookies import SimpleCookie
+
+
+
+from flask_csv import send_csv
 
 app = Flask(__name__)
 
@@ -13,10 +21,64 @@ if __name__ == "__main__":
     app.run()
 
 
-@app.route("/requirements")
+@app.route("/requirements/")
 def requirements():
     req = open("requirements.txt")
-    return render_template('requirements.html', requirements_string=req.read())
+    return render_template('requirements.html', requirements_string=f'{url_for}')
+
+
+@app.route("/generate-users/")
+@app.route("/generate-users/", methods=['GET'])
+def generate_users():
+    user_all_dict = []
+
+    def randomizer():
+        # Список имен
+        name_list = ["Aleksandr", "Sasha", "Alexey", "Alyosha", "Albert", "Alik", "Anatoly", "Andrey", "Anton",
+                     "Antosha", "Arkadiy", "Artem", "Artur", "Arkhip", "Afanasii", "Boris", "Bronislav", "Vadim",
+                     "Valentin", "Valya", "Valeriy", "Vasily", "Viktor", "Vitaly", "Vladimir", "Vladislav", "Vsevolod",
+                     "Vyacheslav"]
+        # Радномайзер числа для списка имен
+        ran_name_number = randint(0, len(name_list) - 1)
+        # Имя из списка под рандомный номером
+        random_first_name = name_list[ran_name_number]
+        # Cписок почтовых ящиков
+        mail_list = ["gmail.com", "yahoo.com", "urk.net"]
+        # Радномайзер числа для списка почтовых ящиков
+        ran_mail_number = randint(0, len(mail_list) - 1)
+        # Почтовый ящик из списка под рандомный номером
+        random_mailbox = mail_list[ran_mail_number]
+        random_name = ''
+        # Генератор имени для почтового ящика, который генерируется только из букв имени
+        for _ in range(7):
+            ran_mail_name_number = randint(0, len(random_first_name) - 1)
+            random_name += random_first_name.lower()[ran_mail_name_number]
+
+        mail_post = random_name + "@" + random_mailbox
+        users_dict = {"User Name": random_first_name, "Email": mail_post}
+
+        return users_dict
+
+    #Парсим
+    args = request.args
+    new_dict = dict(args)
+
+    if new_dict.get('count'):
+        for _ in range(int(new_dict.get('count'))):
+            user_all_dict.append(randomizer())
+        number = int(new_dict.get('count'))
+    else:
+        user_all_dict.append(randomizer())
+        number = 1
+
+    with open("generate_users.txt", 'w') as file:
+        file.write(str(user_all_dict))
+
+    req = open("generate_users.txt")
+    return render_template('generate_users.html', generate_users_string=req.read(), number=number)
+
+
+
 
 
 
