e6caa906d7042ca751103903e1f8065ca43b045e
Pashatishinin <99612110+Pashatishinin@users.noreply.github.com>
2023-01-16 20:52:36 +0100

    Track list

--- main.py
+++ main.py
@@ -1,93 +1,50 @@
-from flask import Flask, render_template, url_for, request
-from random import randint
-import csv
-import requests
-
-app = Flask(__name__)
-
-if __name__ == "__main__":
-    app.run()
+import os
 
 
-@app.route("/requirements/")
-def requirements():
-    req = open("requirements.txt")
-    return render_template('requirements.html', requirements_string=f'{url_for}')
+from flask import Flask, render_template, request
 
 
-@app.route("/generate-users/")
-@app.route("/generate-users/", methods=['GET'])
-def generate_users():
-    user_all_dict = []
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'tracks.sqlite'),
+    )
 
-    def randomizer():
-        # Список имен
-        name_list = ["Aleksandr", "Sasha", "Alexey", "Alyosha", "Albert", "Alik", "Anatoly", "Andrey", "Anton",
-                     "Antosha", "Arkadiy", "Artem", "Artur", "Arkhip", "Afanasii", "Boris", "Bronislav", "Vadim",
-                     "Valentin", "Valya", "Valeriy", "Vasily", "Viktor", "Vitaly", "Vladimir", "Vladislav", "Vsevolod",
-                     "Vyacheslav"]
-        # Радномайзер числа для списка имен
-        ran_name_number = randint(0, len(name_list) - 1)
-        # Имя из списка под рандомный номером
-        random_first_name = name_list[ran_name_number]
-        # Cписок почтовых ящиков
-        mail_list = ["gmail.com", "yahoo.com", "urk.net"]
-        # Радномайзер числа для списка почтовых ящиков
-        ran_mail_number = randint(0, len(mail_list) - 1)
-        # Почтовый ящик из списка под рандомный номером
-        random_mailbox = mail_list[ran_mail_number]
-        random_name = ''
-        # Генератор имени для почтового ящика, который генерируется только из букв имени
-        for _ in range(7):
-            ran_mail_name_number = randint(0, len(random_first_name) - 1)
-            random_name += random_first_name.lower()[ran_mail_name_number]
-
-        mail_post = random_name + "@" + random_mailbox
-        users_dict = {"User Name": random_first_name, "Email": mail_post}
-
-        return users_dict
-
-
-    #Парсим query
-    args = request.args
-    new_dict = dict(args)
-
-    if new_dict.get('count'):
-        for _ in range(int(new_dict.get('count'))):
-            user_all_dict.append(randomizer())
-        number = int(new_dict.get('count'))
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
     else:
-        user_all_dict.append(randomizer())
-        number = 1
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
 
-    with open("generate_users.txt", 'w') as file:
-        file.write(str(user_all_dict))
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
 
-    req = open("generate_users.txt")
-    return render_template('generate_users.html', generate_users_string=req.read(), number=number)
+    import db
+    db.init_app(app)
+
+    import auth
+    app.register_blueprint(auth.bp)
+
+    import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
+
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+    return app
 
 
-@app.route("/mean/")
-def mean():
-    with open('hw.csv', "r", newline='') as csvfile:
-        reader = csv.DictReader(csvfile)
-        count = 0
-        h = 0.0
-        w = 0.0
-        for row in reader:
-            h += float(row.get(' "Height(Inches)"'))
-            w += float(row.get(' "Weight(Pounds)"'))
-            count += 1
-        print(h, w, count)
-        middle_height = (h/count)*2.54
-        middle_weight = (w/count)*0.45
-    return render_template('mean.html', height=middle_height, weight=middle_weight)
 
 
-@app.route("/space/")
-def space():
-    r = requests.get('http://api.open-notify.org/astros.json')
-    r.json()
-    new_dict = dict(r.json())
-    people = new_dict.get('people')
-    return render_template('space.html', number=len(people), people=people)
+
+
+
